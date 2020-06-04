96bd99baae0f2620ecce8b4bd8a64aa928015325
Chris Palka <cpalka87gmail.com>
2020-06-03 21:25:26 -0600

    Complete bare_minimum/promiseConstructor.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,29 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    console.log('DATA: ', data);
+    if (err) {
+      console.log('err:', err);
+      callback(err);
+    } else {
+      data = data.split('\n');
+      callback(null, data[0]);
+    }
+  });
   // TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
+  request(url, function(err, response, body) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
   // TODO
 };
 
